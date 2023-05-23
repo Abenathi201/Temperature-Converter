c3b6ed922689fc6e067ff1e7057119711f135129
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-05-23 16:35:08 +0200

    Done with JS only needs CSS

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 function celcius() {
     let celcius = (document.getElementById("input").value - 32) / 1.8;
     celcius = Math.round(celcius);
-    document.getElementById("output").innerHTML = input.value + " " + "Fahrenheit" + " is " + celcius + " " + "Celsius";
+    document.getElementById("output").innerHTML = input.value + " " + "Fahrenheit" + " is " + celcius + " " + "degrees Celsius";
 }
 
 function fahrenheit() {
     let fahrenheit = document.getElementById("input").value * 1.8 +32;
     fahrenheit = Math.round(fahrenheit);
-    document.getElementById("output").innerHTML = input.value + " " + "Celsius"+ " is " + fahrenheit + " " + "Fahrenheit";
+    document.getElementById("output").innerHTML = input.value + " " + "degrees Celsius"+ " is " + fahrenheit + " " + "Fahrenheit";
 }
