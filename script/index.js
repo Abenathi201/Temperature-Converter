3a3b48e7b5dc461b3c905db155ff774b3c8e6a1a
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-05-25 09:41:49 +0200

    Finished

--- script/index.js
+++ script/index.js
@@ -1,11 +1,17 @@
 function celcius() {
     let celcius = (document.getElementById("input").value - 32) / 1.8;
     celcius = Math.round(celcius);
-    document.getElementById("output").innerHTML = input.value + " " + "Fahrenheit" + " is " + celcius + " " + "degrees Celsius";
+    document.getElementById("output").innerHTML = input.value + " " + "°F" + " is " + celcius + " " + "°C";
+    document.getElementById("header").innerText = "Your Temperature is :"
 }
 
 function fahrenheit() {
     let fahrenheit = document.getElementById("input").value * 1.8 +32;
     fahrenheit = Math.round(fahrenheit);
-    document.getElementById("output").innerHTML = input.value + " " + "degrees Celsius"+ " is " + fahrenheit + " " + "Fahrenheit";
+    document.getElementById("output").innerHTML = input.value + " " + "°C"+ " is " + fahrenheit + " " + "°F";
+    document.getElementById("header").innerText = "Your Temperature is :"
+}
+
+function reload() {
+    window.location.reload();
 }
